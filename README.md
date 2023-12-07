88e627f3a0579d62b115332c2be23654fa9ec888
Marc Bruni <marc.bruni@students.fhnw.ch>
2023-12-07 22:53:57 +0900

    Modularized the notebook

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # NLP Project 2023
 
-This repository contains the Natural Language Processing Project at Seoul National University
+This repository contains our Natural Language Processing Term Project at Seoul National University
 
 ## Used Dataset
 
@@ -17,15 +17,10 @@ We use the following library to perform a casual to formal style transfer on the
 * [https://huggingface.co/rajistics/informal_formal_style_transfer](https://huggingface.co/rajistics/informal_formal_style_transfer)
 * [https://github.com/PrithivirajDamodaran/Styleformer#active-to-passive--available-now---](https://github.com/PrithivirajDamodaran/Styleformer#active-to-passive--available-now---)
 
-We then report score for on the test set without with style transfer.
+We then report score for on the test set without and with style transfer.
 
 The hypothesis is that the classification of fake news gets worse.
 
 ## TODO
 
-Currently the Jupyter Notebook takes too long to run. We need to modularize the code and create scripts/notebooks that can be run independently.
-
-* [ ] Create notebook that creates test and train set and performs style transfer on test set
-* [ ] Create notebook that finetunes Bert Classifier on the dataset using the test/train set of the first notebook
-* [ ] Create notebook that evaluates the Classifier on the test set with and without style transfer
 * [ ] Ensure reproducibility by using the same seed everywhere (there seems to be no seed somewhere - maybe torch)
---
+++ shared.py
@@ -0,0 +1,55 @@
+from pathlib import Path
+
+import torch
+# TODO from google.colab import drive
+from torch.utils.data import TensorDataset
+from transformers import (
+    BertTokenizer,
+)
+
+BERT_MODEL = "bert-base-uncased"
+tokenizer = BertTokenizer.from_pretrained(BERT_MODEL)
+SEED = 42
+
+WORKDIR = Path(".")
+DATADIR = WORKDIR / "data"
+MODELDIR = WORKDIR / "model_old"
+
+train_dataset_path = DATADIR / 'train_dataset.pt'
+test_dataset_path = DATADIR / 'test_dataset.pt'
+tokenized_train_dataset_path = DATADIR / 'train_dataset_tokenized.pt'
+tokenized_test_dataset_path = DATADIR / 'test_dataset_tokenized.pt'
+untransferred_testset_path = DATADIR / 'untransferred_testset.pt'
+transferred_testset_path = DATADIR / 'transferred_testset.pt'
+model_path = MODELDIR / 'BERT_finetuned.ckpt'
+
+def tokenize_texts(df, feature="all_text"):
+    input_ids = []
+    attention_masks = []
+
+    for text in df[feature]:
+        encoded_dict = tokenizer.encode_plus(
+            text,
+            add_special_tokens=True,
+            max_length=64,
+            padding="max_length",
+            return_attention_mask=True,
+            return_tensors="pt",
+            truncation=True,
+        )
+
+        input_ids.append(encoded_dict["input_ids"])
+
+        attention_masks.append(encoded_dict["attention_mask"])
+
+    input_ids = torch.cat(input_ids, dim=0)
+    attention_masks = torch.cat(attention_masks, dim=0)
+
+    return input_ids, attention_masks
+
+
+def create_tensor_dataset(df, feature="all_text"):
+    input_ids, attention_masks = tokenize_texts(df, feature=feature)
+    labels = torch.tensor(df["label"].values)
+
+    return TensorDataset(input_ids, attention_masks, labels)
