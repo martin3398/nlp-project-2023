da91e79a15d901264c172aa3ee0662f608dabdcd
iisabelaaa <79070073+iisabelaaa@users.noreply.github.com>
2023-12-09 15:32:43 +0900

    Update shared.py

--- shared.py
+++ shared.py
@@ -5,10 +5,20 @@ from torch.utils.data import TensorDataset
 from transformers import (
     BertTokenizer,
 )
+import random
+import numpy as np
+
+def set_seed(seed_value=42):
+    random.seed(seed_value)
+    np.random.seed(seed_value)
+    torch.manual_seed(seed_value)
+    torch.cuda.manual_seed_all(seed_value)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
 
 BERT_MODEL = "bert-base-uncased"
 tokenizer = BertTokenizer.from_pretrained(BERT_MODEL)
-SEED = 42
+set_seed()
 
 WORKDIR = Path(".")
 DATADIR = WORKDIR / "data"
